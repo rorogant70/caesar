d08594222916b9992c4eb3a76c6ac3ed76ec2a8d
rorogant70 <ronagurl19@gmail.com>
2017-08-25 10:31:30 -0400

    Update and rename .gitignore to caesar.py

---
+++ .gitignore/caesar.py
@@ -0,0 +1,15 @@
+def caesar(shift, plaintext)
+# you own code here
+
+ letters= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+ x= input("type 3, 6, or 9")
+for x
+    if input = 3:
+    print ('d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',)
+
+    if input= 6:
+    print ('i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+
+else
+    print ("[letters]")
+ 
